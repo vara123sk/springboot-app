aff7791b9310fa0e5955c3cdb45979754cc1eeb2
vara <vara123sk@gmail.com>
2025-12-06 22:59:26 +0530

    test

--- src/main/java/com/demo/app/controller/HelloController.java
+++ src/main/java/com/demo/app/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello from Sample app");
+        return ResponseEntity.ok("Hello from sample app");
     }
 }
